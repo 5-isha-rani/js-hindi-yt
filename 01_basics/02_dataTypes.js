5d11b2baf9f50801a33c73a017b0ee24c42e49b2
5-isha-rani <113941185+5-isha-rani@users.noreply.github.com>
2023-10-02 06:26:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version 
+//alert (3 + 3) // we are using nodejs , not browser
+
+//null means an empty value
+let age = null;
+console.log(age);
+//number
+//string  
+//boolean
+//null  => empty value
+//undefined
+//symbol =>unique
+
+//object
