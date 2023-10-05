80b095172c848be1c470d64b77b5f8f8d51175be
5-isha-rani <113941185+5-isha-rani@users.noreply.github.com>
2023-10-05 14:26:57 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,35 @@
+const user = {
+    username : "isha",
+    price: 999,
+
+    welcomeMsg : function(){
+        console.log(`${this.username} ,welcome`);
+        // console.log(this) //{}
+    }
+}
+// user.welcomeMsg()
+user.username = "ishuuu"
+// user.welcomeMsg()
+// console.log(this)
+
+
+function chai(){
+    const username = "isha";
+    console.log(this.username);//undefined
+
+console.log(this);  
+}
+// chai()
+
+const chai2 = ()=>{
+    const username = "isha";
+    console.log(this);//{}
+}
+// chai2();
+
+//implicit return in arrow function
+const addTwo = (num1,num2)=> (num1+num2)
+// console.log(addTwo(2,3))
+
+const fun = ()=>({username:"isha"})
+console.log(fun())
