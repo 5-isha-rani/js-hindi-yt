532e4e28ded81a53bd808e5e64f1746acf55927a
5-isha-rani <113941185+5-isha-rani@users.noreply.github.com>
2023-10-07 17:08:42 +0000

    reduce

---
+++ 05_iterations/03_three.js
@@ -0,0 +1,18 @@
+//map function in array
+const num = [1,2,3,4,5];
+
+const newNum = num.map((item , index , num)=> item+10);
+// console.log(newNum);
+
+const num2 = num.map(x=> x*10 )
+.map(x => x+1)
+.filter(x => x>30)
+// console.log(num2);
+
+const num3 = [1,2,3,4,5]
+
+const initial_value = 0;
+let res = num3.reduce(x => , initial_value);
+console.log(res);
+
+
